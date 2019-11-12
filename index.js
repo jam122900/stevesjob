e701323391f0cc2f8a51e8a14bd72d32b6c6250d
jam122900 <57652531+jam122900@users.noreply.github.com>
2019-11-11 22:58:36 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function playSound() {
 	console.log("playing");
-	var paths = ["sound1.mp3, sound2.mp3"];
-	var snd = new Audio(path[Math.floor(Math.random() * paths.length)]);
-	snd.play();
+	var paths = ["sound (1).wav", "sound (2).wav", "sound (3).wav", "sound (4).wav", "sound (5).wav", "sound (6).wav", "sound (7).wav", "sound (8).wav", "sound (9).wav", "sound (10).wav", "sound (11).wav", "sound (12).wav",];
+	var snd = new Pizzicato.Sound(paths[Math.floor(Math.random() * paths.length)], function() {
+		// Sound loaded!
+		snd.play();
+	});
 }
